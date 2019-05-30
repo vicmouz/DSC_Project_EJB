578d119919a95dd3e09552b8781ce07c093832fb
Marcos <mbff1998@gmail.com>
2019-05-29 21:38:59 -0300

    cor, imagem, tipo e tamanho feitos

---
+++ LojaVirtual_ejb/src/test/java/softwarecorporativo/ejb/test/CorProdutoTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwarecorporativo.ejb.test;
+
+import javax.naming.NamingException;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import softwarecorporativo.entidade.CorProduto;
+import softwarecorporativo.servico.CorProdutoServico;
+
+/**
+ *
+ * @author marcosbrasil98
+ */
+public class CorProdutoTest extends Teste {
+    
+    private CorProdutoServico corProdutoServico;
+    
+    @Before
+    public void setUp() throws NamingException {
+        corProdutoServico = (CorProdutoServico) container.getContext().lookup("java:global/classes/ejb/CorProdutoServico!softwarecorporativo.servico.CorProdutoServico");
+    }
+    @After
+    public void tearDown() {
+        corProdutoServico = null;
+    }
+    
+    
+    
+    @Test
+    public void existeCor() {
+        CorProduto corProduto = corProdutoServico.criar();
+        corProduto.setTipo("Caneca");
+        assertTrue(corProdutoServico.existe(corProduto));
+    }
+    
+    @Test
+    public void getCorPorTipo() {
+        CorProduto corProduto = corProdutoServico.consultarPorTipo("Camisa");
+        assertNotNull(corProduto);
+        assertEquals("Amarelo", corProduto.getNome());
+    }
+    
+   
+    
+    @Test
+    public void getCorPorId() {
+        assertNotNull(corProdutoServico.consultarPorId(new Long(2)));
+    }
+    
+    @Test
+    public void persistir() {   
+        
+        CorProduto corProduto = corProdutoServico.criar();
+        corProduto.setId(6l);
+        corProduto.setNome("Azulado");
+        corProduto.setTipo("Teste");
+        corProdutoServico.persistirCorProduto(corProduto);
+        assertNotNull(corProduto.getId());
+        
+    }
+    
+    @Test
+    public void atualizar() {
+        CorProduto corProduto = corProdutoServico.consultarPorId(new Long(2));
+        corProduto.setNome("Cor nova"); 
+        corProdutoServico.atualizar(corProduto);
+        corProduto = corProdutoServico.consultarPorId(new Long(2));
+        assertEquals("Cor nova", corProduto.getNome());
+    }
+    
+}
---
+++ LojaVirtual_ejb/src/test/java/softwarecorporativo/ejb/test/TamanhoProdutoTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwarecorporativo.ejb.test;
+
+import javax.naming.NamingException;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import softwarecorporativo.entidade.TamanhoProduto;
+import softwarecorporativo.servico.TamanhoServico;
+
+/**
+ *
+ * @author marcosbrasil98
+ */
+public class TamanhoProdutoTest extends Teste {
+   
+     private TamanhoServico tamanhoservico;
+    
+    @Before
+    public void setUp() throws NamingException {
+        tamanhoservico = (TamanhoServico) container.getContext().lookup("java:global/classes/ejb/TamanhoServico!softwarecorporativo.servico.TamanhoServico");
+    }
+    @After
+    public void tearDown() {
+        tamanhoservico = null;
+    }
+    
+    
+    
+    @Test
+    public void existeTamanho() {
+        TamanhoProduto tamanhoProduto = tamanhoservico.criar();
+        tamanhoProduto.setTipo("Caneca");
+        assertTrue(tamanhoservico.existe(tamanhoProduto));
+    }
+    
+    @Test
+    public void getTamanhoPorTipo() {
+        TamanhoProduto tamanhoProduto = tamanhoservico.consultarPorTipo("Camisa");
+        assertNotNull(tamanhoProduto);
+        assertEquals("Médio", tamanhoProduto.getNome());
+    }
+    
+   
+    
+    @Test
+    public void getTamanhoPorId() {
+        assertNotNull(tamanhoservico.consultarPorId(new Long(2)));
+    }
+    
+    @Test
+    public void persistir() {   
+        
+        TamanhoProduto tamanhoProduto = tamanhoservico.criar();
+        tamanhoProduto.setId(9l);
+        tamanhoProduto.setAltura(23.4);
+        tamanhoProduto.setLargura(23.1);
+        tamanhoProduto.setNome("Teste");
+        tamanhoProduto.setTipo("Testinho");
+        tamanhoservico.persistirTamanhoProduto(tamanhoProduto);
+        assertNotNull(tamanhoProduto.getId());
+        
+    }
+    
+    @Test
+    public void atualizar() {
+        TamanhoProduto tamanhoProduto = tamanhoservico.consultarPorId(new Long(2));
+        tamanhoProduto.setNome("Sei lá"); 
+        tamanhoservico.atualizar(tamanhoProduto);
+        tamanhoProduto = tamanhoservico.consultarPorId(new Long(2));
+        assertEquals("Sei lá", tamanhoProduto.getNome());
+    }
+    
+   
+    
+    
+}
